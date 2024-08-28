6275b3e8acbfe68d51407da4263f86d1393ba033
h4sski.programming <h4sski.programming@gmail.com>
2024-08-28 23:28:29 +0200

    moving forward, trying to pass formated text from view to template

--- h4sski_site/h4site/urls.py
+++ h4sski_site/h4site/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
+from h4site.views import IndexView, SubView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', IndexView.as_view(), name="index"),
+    path('sub', SubView.as_view(), name="sub"),
 ]
