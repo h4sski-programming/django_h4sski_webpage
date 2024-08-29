c44e18ca2912a715d120b0edf6d0f7b1dc56f37a
h4sski.programming <h4sski.programming@gmail.com>
2024-08-29 11:19:31 +0200

    fulfilling page as hardcoded values from views, have 2 layouts

--- h4sski_site/h4site/urls.py
+++ h4sski_site/h4site/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from h4site.views import IndexView, SubView
+from h4site.views import IndexView, Index2View, SubView
 
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', IndexView.as_view(), name="index"),
+    path('2', Index2View.as_view(), name="index2"),
     path('sub', SubView.as_view(), name="sub"),
 ]
